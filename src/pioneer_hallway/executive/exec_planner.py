763109061019e7a99c3abf15007d5e8b7f37785f
William Doyle <williamdoyleiii@gmail.com>
2017-02-27 14:14:06 -0500

    update to include -timeout flag for testing

--- src/pioneer_hallway/executive/exec_planner.py
+++ src/pioneer_hallway/executive/exec_planner.py
@@ -2,4 +2,13 @@
 
 import subprocess
 
-p = subprocess.Popen("./planner.sh", shell=True, stdin=subprocess.PIPE)
+cur_x = 1.0
+cur_y = 1.0
+cur_lin = 0.0
+cur_rot = 0.0
+
+p = subprocess.Popen("./planner.sh", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+# timestamp beginning header
+p.stdin.write(str(time.time())
+p.stdin.write(str(
