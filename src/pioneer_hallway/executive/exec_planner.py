8f3290fbcf6c837c2f3e99b1314deceb29f6b475
William Doyle <williamdoyleiii@gmail.com>
2017-02-27 19:00:56 -0500

    made exec_planner faster

--- src/pioneer_hallway/executive/exec_planner.py
+++ src/pioneer_hallway/executive/exec_planner.py
@@ -12,24 +12,19 @@ from nbstreamreader import NonBlockingStreamReader as NBSR
 Obstacle = namedtuple("Obstacle", "x y")
 ObstacleDb = []
 
+# callbacks
+def obstacle_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "obstacle_msg %s", data.data)
+
+# initialize our ROS nodes here
+pub = rospy.Publisher('controller_msg', String, queue_size=1)
+rospy.init_node('executive', anonymous=True)
+rate = rospy.Rate(10)
+rospy.Subscriber('obst_tracker', String, obstacle_callback)
+
 def exec_control_pub(action):
-    pub = rospy.Publisher('controller_msg', String, queue_size=1)
-    rospy.init_node('exec_control_pub', anonymous=True)
-    print("exec_control_pub sending action to controller...")
-    rate = rospy.Rate(10)
     rospy.loginfo(action)
     pub.publish(action)
-    rate.sleep()
-
-# TODO: find out the obst type and fill in Db
-def obstacle_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "obstacle_msg: %s", data.data)
-
-def obstacle_listener():
-    rospy.init_node('exec_obs_listen', anonymous=True)
-    rospy.Subscriber('obst_tracker', String, obstacle_callback)
-    print("exec_obs_listen waiting for obstacles...")
-    rospy.spin()
 
 # instance variables used to keep track of current state
 # update these when we do forward projection
@@ -46,31 +41,30 @@ def send_msg_to_planner(master_clock, p, nbsr):
     for obst in ObstacleDb:
         msg = msg + str(obst.x) + ' ' + str(obst.y) + '\n'
     msg = msg + "END\n"
-    print("SENT TO PLANNER:\n" + msg)
-    action = ""
     p.stdin.write(msg)
-    time.sleep(0.3)
+    # have to wait for the process to respond
     try:
-        return (time.time(), nbsr.readline(0.3))
+        return (time.time(), nbsr.readline(0.350))
     except:
         return (time.time(), "")
 
 if __name__ == '__main__':
     # fork and create a child subprocess of the planner
-    planner = subprocess.Popen("./planner.sh -timeout 300", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("Running Planner with 3s startup time...")
+    planner = subprocess.Popen("./planner.sh -timeout 250", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     nbsr = NBSR(planner.stdout)
     # give time for the planner to initialize
     time.sleep(3)
     # the master clock for the planner
     master_clock = time.time()
+    cur_clock = time.time()
     try:
-        while (master_clock - time.time() < 300):
+        while ((time.time() - cur_clock) < 1.0):
+            print("ellasped time: " + str(time.time()-cur_clock))
             # send the msg to the planner store the time it took
-            master_clock, action = send_msg_to_planner(master_clock, planner, nbsr)
-            print(action)
-            print("cur_state: " + print_cur_state())
+            cur_clock, action = send_msg_to_planner(master_clock, planner, nbsr)
             exec_control_pub(action)
-            master_clock = time.time()
+            master_clock = cur_clock
     except rospy.ROSInterruptException:
         print("ESTOP")
         pass
