3af910baf606e8d8a282f6748f1cbb38a372454d
Estuardo Carpio <eccarpio@hotmail.com>
2017-04-22 17:51:00 -0400

    added custom amcl package that performs updates based on odometry data updated rviz configuration file updated simulation configuration file to add odometry threshold parameter fixed estop publishing bugs lowered laser publish rate to enhance simulation modified number of particles in amcl to enchance simulation

--- src/pioneer_hallway/estop/src/estop.py
+++ src/pioneer_hallway/estop/src/estop.py
@@ -3,9 +3,9 @@
 import rospy
 import roslib
 import math
-from tf import TransformListener, transformations
 import subprocess
 import numpy as np
+from tf import TransformListener, transformations
 from sensor_msgs.msg import LaserScan, PointCloud
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped, Point32
@@ -16,10 +16,11 @@ from std_srvs.srv import Empty
 rosAriaPose = Odometry()
 amclPose = PoseWithCovarianceStamped()
 disablePublisher = None
-#testCloudPub = None
-#currentFramePub = None
+collisionFramePub = None
 predictedFramePub = None
 cmdVelPub = None
+collision = False
+#testCloudPub = None
 
 #used in rotations
 frame = 'map'
@@ -41,7 +42,11 @@ maxDecel = 0.6
 #lineEnd and lineStart define the end and start points of the line
 #point is the point we want to calculate the distance to
 def calculateDistance(lineEnd, lineStart, point):
-	distance = abs((lineEnd.y - lineStart.y) * point.x - (lineEnd.x - lineStart.x) * point.y + lineEnd.x * lineStart.y - lineEnd.y * lineStart.x) / pow(pow(lineEnd.y - lineStart.y,2) + pow(lineEnd.x - lineStart.x,2), 1/2)
+	distance = (abs((lineEnd.y - lineStart.y) * point.x - 
+	   (lineEnd.x - lineStart.x) * point.y + 
+	   lineEnd.x * lineStart.y - lineEnd.y * lineStart.x) / 
+	   pow(pow(lineEnd.y - lineStart.y,2) + 
+	   pow(lineEnd.x - lineStart.x,2), 1/2))
 	return distance
 
 #calculates the distance between two points
@@ -49,14 +54,17 @@ def calculateEuclideanDistance(p1, p2):
 	return sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
 
 def polar_to_euclidean( angles, ranges ):
-    return [ [ np.cos(theta)*r, np.sin(theta)*r ] for theta, r in zip( angles, ranges ) ]
+    return [ [ np.cos(theta)*r, np.sin(theta)*r ] for 
+           theta, r in zip( angles, ranges ) ]
 
 #rotates a point given a quaternion
 #pin is the point that will be rotated
 #q is the quaternion that will be used to ratet the point
 def rotatePose(pin, q):
-	x = q.w * q.w * pin.x - 2 * q.z * q.w * pin.y + q.x * q.x * pin.x + 2 * q.y *q.x * pin.y - q.z * q.z * pin.x - q.y * q.y * pin.x;
-	y = 2 * q.x * q.y * pin.x + q.y * q.y * pin.y + 2 * q.w * q.z * pin.x - q.z *q.z * pin.y + q.w * q.w * pin.y - q.x * q.x * pin.y;
+	x = (q.w * q.w * pin.x - 2 * q.z * q.w * pin.y + q.x * q.x * pin.x + 
+	    2 * q.y *q.x * pin.y - q.z * q.z * pin.x - q.y * q.y * pin.x)
+	y = (2 * q.x * q.y * pin.x + q.y * q.y * pin.y + 2 * q.w * q.z * pin.x - 
+	    q.z *q.z * pin.y + q.w * q.w * pin.y - q.x * q.x * pin.y)
 	pin.x = x
 	pin.y = y
 
@@ -77,7 +85,7 @@ def calculatePolygons(deltaT):
 	currTwist = rosAriaPose.twist.twist
 	
 	#create polygons
-	polyCurr = PolygonStamped()
+	#polyCurr = PolygonStamped()
 	polyPred = PolygonStamped()
 	
 	#rotate robot frame
@@ -91,10 +99,10 @@ def calculatePolygons(deltaT):
 	rotatePose(robotFrameD, currPose.orientation)
 	
 	#calculate current vertices
-	cp0 = Point32(currPose.position.x + robotFrameA.x, currPose.position.y + robotFrameA.y, 0)
-	cp1 = Point32(currPose.position.x + robotFrameB.x, currPose.position.y + robotFrameB.y, 0)
-	cp2 = Point32(currPose.position.x + robotFrameC.x, currPose.position.y + robotFrameC.y, 0)
-	cp3 = Point32(currPose.position.x + robotFrameD.x, currPose.position.y + robotFrameD.y, 0)
+	#cp0 = Point32(currPose.position.x + robotFrameA.x, currPose.position.y + robotFrameA.y, 0)
+	#cp1 = Point32(currPose.position.x + robotFrameB.x, currPose.position.y + robotFrameB.y, 0)
+	#cp2 = Point32(currPose.position.x + robotFrameC.x, currPose.position.y + robotFrameC.y, 0)
+	#cp3 = Point32(currPose.position.x + robotFrameD.x, currPose.position.y + robotFrameD.y, 0)
 	
 	#predict vertices
 	#calculates linear movement
@@ -108,18 +116,22 @@ def calculatePolygons(deltaT):
 	rotatePose(robotFrameB, newOrientation)
 	rotatePose(robotFrameC, newOrientation)
 	rotatePose(robotFrameD, newOrientation)
-	pp0 = Point32(currPose.position.x + robotFrameA.x + delta.x, currPose.position.y + robotFrameA.y + delta.y, 0)
-	pp1 = Point32(currPose.position.x + robotFrameB.x + delta.x, currPose.position.y + robotFrameB.y + delta.y, 0)
-	pp2 = Point32(currPose.position.x + robotFrameC.x + delta.x, currPose.position.y + robotFrameC.y + delta.y, 0)
-	pp3 = Point32(currPose.position.x + robotFrameD.x + delta.x, currPose.position.y + robotFrameD.y + delta.y, 0)
+	pp0 = Point32(currPose.position.x + robotFrameA.x + delta.x, 
+	       currPose.position.y + robotFrameA.y + delta.y, 0)
+	pp1 = Point32(currPose.position.x + robotFrameB.x + delta.x, 
+	       currPose.position.y + robotFrameB.y + delta.y, 0)
+	pp2 = Point32(currPose.position.x + robotFrameC.x + delta.x, 
+	       currPose.position.y + robotFrameC.y + delta.y, 0)
+	pp3 = Point32(currPose.position.x + robotFrameD.x + delta.x, 
+	       currPose.position.y + robotFrameD.y + delta.y, 0)
 	
 	#create final polygons
-	polyCurr.polygon.points = [None] * 4
-	polyCurr.polygon.points[0] = cp0
-	polyCurr.polygon.points[1] = cp1
-	polyCurr.polygon.points[2] = cp3
-	polyCurr.polygon.points[3] = cp2
-	polyCurr.header.frame_id = frame
+	#polyCurr.polygon.points = [None] * 4
+	#polyCurr.polygon.points[0] = cp0
+	#polyCurr.polygon.points[1] = cp1
+	#polyCurr.polygon.points[2] = cp3
+	#polyCurr.polygon.points[3] = cp2
+	#polyCurr.header.frame_id = frame
 	
 	polyPred.polygon.points = [None] * 4
 	polyPred.polygon.points[0] = pp0
@@ -130,12 +142,15 @@ def calculatePolygons(deltaT):
 	
 	#publish polygons
 	#currentFramePub.publish(polyCurr)
-	#predictedFramePub.publish(polyPred)
+	predictedFramePub.publish(polyPred)
 
 	return polyPred
 
-def triggerEstop():
+def triggerEstop(polygon):
+	global collision
+	collision = True
 	disablePublisher()
+	collisionFramePub.publish(polygon)
 	rospy.loginfo('Possible collision detected.')
 	subprocess.call(['/usr/bin/canberra-gtk-play','--id','suspend-error'])
 	#possible sound files are in /usr/share/sounds/freedesktop/stereo
@@ -143,100 +158,116 @@ def triggerEstop():
 
 #uses sonar readings to predict collisions
 def sonarCallback(data):
-	pose = amclPose.pose.pose
-	pc = PointCloud()
-	pc.header.frame_id = "map"
-	pc.points = [0] * len(data.points)
-	counter = 0
-	for point in data.points:
-		tfq = transformations.quaternion_from_euler(0, 0, 0)
-		q = Quaternion(tfq[0], tfq[1], tfq[2], tfq[3])
-		p = Point32(point.x, point.y, 0)
-		rotatePose(p, q)
-		#p.x = p.x
-		rotatePose(p, pose.orientation)
-		p.x = pose.position.x + p.x
-		p.y = pose.position.y + p.y
-		pc.points[counter] = p
-		counter = counter + 1
-	#testCloudPub.publish(pc)
-	evaluateReadings(pc)
+	global collision
+	currSpeed = rosAriaPose.twist.twist
+	if not collision and (currSpeed.linear.x >= abs(0.02)):
+		pose = amclPose.pose.pose
+		pc = PointCloud()
+		pc.header.frame_id = "map"
+		pc.points = [0] * len(data.points)
+		counter = 0
+		for point in data.points:
+			tfq = transformations.quaternion_from_euler(0, 0, 0)
+			q = Quaternion(tfq[0], tfq[1], tfq[2], tfq[3])
+			p = Point32(point.x, point.y, 0)
+			rotatePose(p, q)
+			#p.x = p.x
+			rotatePose(p, pose.orientation)
+			p.x = pose.position.x + p.x
+			p.y = pose.position.y + p.y
+			pc.points[counter] = p
+			counter = counter + 1
+		#testCloudPub.publish(pc)
+		if not collision:
+			evaluateReadings(pc)
 
 #uses laser readings to predict collisions
 def laserCallback(data):
-	pose = amclPose.pose.pose
-	points = polar_to_euclidean( [ data.angle_min + (i * data.angle_increment)  for i in range(len(data.ranges))], data.ranges)
-	pc = PointCloud()
-	pc.header.frame_id = "map"
-	pc.points = [0] * len(points)		
-	counter = 0
-	for point in points:
-		p = Point32(laserOffset + point[0], point[1], 0)
-		rotatePose(p, pose.orientation)
-		p.x = pose.position.x + p.x
-		p.y = pose.position.y + p.y
-		pc.points[counter] = p
-		counter = counter + 1			
-	evaluateReadings(pc)
-	#testCloudPub.publish(pc)
+	global collision
+	currSpeed = rosAriaPose.twist.twist
+	if not collision and (currSpeed.linear.x >= abs(0.02)):
+		pose = amclPose.pose.pose
+		points = polar_to_euclidean( [ data.angle_min + (i * data.angle_increment)  
+							for i in range(len(data.ranges))], data.ranges)
+		pc = PointCloud()
+		pc.header.frame_id = "map"
+		pc.points = [0] * len(points)		
+		counter = 0
+		for point in points:
+			p = Point32(laserOffset + point[0], point[1], 0)
+			rotatePose(p, pose.orientation)
+			p.x = pose.position.x + p.x
+			p.y = pose.position.y + p.y
+			pc.points[counter] = p
+			counter = counter + 1			
+		if not collision:
+			evaluateReadings(pc)
+		#testCloudPub.publish(pc)
 
 #uses laser readings to simulate the sonar and predict collisions
 def sonarLaserCallback(data):
-	pose = amclPose.pose.pose
-	points = polar_to_euclidean( [ data.angle_min + (i * data.angle_increment)  for i in range(len(data.ranges))], data.ranges)
-	pc = PointCloud()
-	pc.header.frame_id = "map"
-	pc.points = [0] * len(points)
-	counter = 0
-	for point in points:
-		tfq = transformations.quaternion_from_euler(0, 0, 3.14)
-		q = Quaternion(tfq[0], tfq[1], tfq[2], tfq[3])
-		p = Point32(point[0], point[1], 0)
-		rotatePose(p, q)
-		p.x = sonarOffset + p.x
-		rotatePose(p, pose.orientation)
-		p.x = pose.position.x + p.x
-		p.y = pose.position.y + p.y
-		pc.points[counter] = p
-		counter = counter + 1
-	evaluateReadings(pc)
-	#testCloudPub.publish(pc)
+	global collision
+	currSpeed = rosAriaPose.twist.twist
+	if not collision and (currSpeed.linear.x >= abs(0.02)):
+		pose = amclPose.pose.pose
+		points = polar_to_euclidean( [ data.angle_min + (i * data.angle_increment)  
+							for i in range(len(data.ranges))], data.ranges)
+		pc = PointCloud()
+		pc.header.frame_id = "map"
+		pc.points = [0] * len(points)
+		counter = 0
+		for point in points:
+			tfq = transformations.quaternion_from_euler(0, 0, 3.14)
+			q = Quaternion(tfq[0], tfq[1], tfq[2], tfq[3])
+			p = Point32(point[0], point[1], 0)
+			rotatePose(p, q)
+			p.x = sonarOffset + p.x
+			rotatePose(p, pose.orientation)
+			p.x = pose.position.x + p.x
+			p.y = pose.position.y + p.y
+			pc.points[counter] = p
+			counter = counter + 1
+		if not collision:
+			evaluateReadings(pc)
+		#testCloudPub.publish(pc)
 
 def evaluateReadings(pc):
 	#calculate deltaT
+	global collision
 	currSpeed = rosAriaPose.twist.twist
 	if currSpeed.linear.x <= -0.02 or currSpeed.linear.x >= 0.02:
-		#rospy.loginfo("%f", currSpeed.linear.x)
 		deltaT = max(abs((currSpeed.linear.x) / maxDecel), 0.5)
 		timeIncrement = deltaT / 4
 		currTime = timeIncrement
-		while currTime <= deltaT:
+		while currTime <= deltaT and not collision:
 			polygon = calculatePolygons(currTime)
-			A = Point32(polygon.polygon.points[1].x - polygon.polygon.points[0].x, polygon.polygon.points[1].y - polygon.polygon.points[0].y, 0)
-			B = Point32(polygon.polygon.points[3].x - polygon.polygon.points[0].x, polygon.polygon.points[3].y - polygon.polygon.points[0].y, 0)			
+			A = Point32(polygon.polygon.points[1].x - polygon.polygon.points[0].x, 
+			     polygon.polygon.points[1].y - polygon.polygon.points[0].y, 0)
+			B = Point32(polygon.polygon.points[3].x - polygon.polygon.points[0].x, 
+			     polygon.polygon.points[3].y - polygon.polygon.points[0].y, 0)			
 			Asquared = pow(A.x,2) + pow(A.y,2)
 			Bsquared = pow(B.x,2) + pow(B.y,2)
 			for point in pc.points:
-				p = Point32(point.x - polygon.polygon.points[0].x, point.y - polygon.polygon.points[0].y, 0)
+				p = Point32(point.x - polygon.polygon.points[0].x, 
+				     point.y - polygon.polygon.points[0].y, 0)
 				if (0 <= p.x * A.x + p.y * A.y <= Asquared):
 					if (0 <= p.x * B.x + p.y * B.y <= Bsquared):	
-						triggerEstop()
+						triggerEstop(polygon)
 			currTime = currTime + timeIncrement
 		predictedFramePub.publish(polygon)
 
 def estop():
 	global disablePublisher
 	global predictedFramePub
-	#global currentFramePub
-	#global testCloudPub
+	global collisionFramePub
 	global cmdVelPub	
-	global transformer
+	#global testCloudPub
 	
 	#initialize node
 	rospy.init_node('pioneer_estop', anonymous=False)
 	
 	#initialize frame publishers
-	#currentFramePub = rospy.Publisher('estop_current_frame', PolygonStamped, queue_size=1)
+	collisionFramePub = rospy.Publisher('estop_collision_frame', PolygonStamped, queue_size=1)
 	predictedFramePub = rospy.Publisher('estop_predicted_frame', PolygonStamped, queue_size=1)
 	#testCloudPub = rospy.Publisher('testCloud', PointCloud, queue_size=1)
 	
@@ -248,13 +279,11 @@ def estop():
 	rospy.wait_for_service('disable_cmd_vel_publisher')
 	
 	#subscribe to laser, sonar, amcl and odometry
-	transformer = TransformListener()
 	rospy.Subscriber(laserTopic, LaserScan, laserCallback)
 	rospy.Subscriber('RosAria/pose', Odometry, rosAriaPoseCallback)
 	rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, amclPoseCallback)
 	if sim == True:
 		rospy.Subscriber('sonar_b', LaserScan, sonarLaserCallback)
-		rospy.loginfo('simulation')
 	else:
 		rospy.Subscriber('RosAria/sonar', PointCloud, sonarCallback)
 		
