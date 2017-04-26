b901231331fcbcb617dc61cf9743100a8d25d26f
William Doyle <williamdoyleiii@gmail.com>
2017-04-26 16:59:44 -0400

    non-proj exec pub to controller change

--- src/pioneer_hallway/executive/exec_planner.py
+++ src/pioneer_hallway/executive/exec_planner.py
@@ -111,7 +111,7 @@ def obstacles(dt, steps):
 def exec_control_pub(action):
     pub.publish(action)
 
-def update_cur(action):
+def update_cur(action, plan):
     rospy.logdebug("update_cur action: " + str(action))
     planner_action = action[0]
     state = (str(predicted_pose[0]), str(predicted_pose[1]), str(vel), str(predicted_pose[2]))
@@ -121,7 +121,9 @@ def update_cur(action):
     cur_primitive = primitives[planner_action]
     rospy.logdebug(str(cur_primitive.wa) + " " + str(cur_primitive.name) + " " + str(planner_action))
     rospy.logdebug(str(predicted_pose[0]) + " " + str(predicted_pose[1]) + " " + str(vel) + " " + str(predicted_pose[2]))
-    project_pose = cur_primitive.apply(predicted_pose[0], predicted_pose[1], vel, 0, predicted_pose[2])
+    #project_pose = cur_primitive.apply(predicted_pose[0], predicted_pose[1], vel, 0, predicted_pose[2])
+    next_state = plan[1].split(' ', 4)
+     
     p_pose = action[1].split(' ', 4)
 
     plan_pose = Pose()
@@ -135,10 +137,11 @@ def update_cur(action):
     plannerPoses.poses.append(plan_pose)
     plannerPosesPub.publish(plannerPoses)
 
-
     global projected_pose
+    projected_pose = (float(next_state[1]), float(next_state[2]), float(next_state[3]), float(next_state[4]))
     #projected_pose= (project_pose[0], project_pose[1], project_pose[3], project_pose[2]) 
-    projected_pose = (float(p_pose[0]), float(p_pose[1]), float(p_pose[2]), float(p_pose[3]))
+    #projected_pose = (float(p_pose[0]), float(p_pose[1]), float(p_pose[2]), float(p_pose[3]))
+    print(projected_pose)
     
 
 def print_projected_pose(delimiter):
@@ -286,7 +289,7 @@ if __name__ == '__main__':
     projected_pose = (predicted_pose[0], predicted_pose[1], vel, predicted_pose[2])
     rospy.loginfo("Executive online...")
     try:
-        while (0.29 >= (time.time() - cur_clock)):
+        while (0.25 >= (time.time() - cur_clock)):
             cur_clock = time.time() 
             if first_iteration:
               t_time = t_time + 250
@@ -295,11 +298,11 @@ if __name__ == '__main__':
             time.sleep(0.240)
             #check for the action to be in the queue
             (action, t_time, projection, plan) = check_planner_for_msg(planner, nbsr)
-            update_cur(action)
+            update_cur(action, plan)
 #            rospy.loginfo("planner_time: " + str(planner_start_time - end_time))
-            rospy.loginfo("action_from_planner: " + action[0])
+            rospy.logdebug("action_from_planner: " + action[0])
             cont_msg = action[0] + "," + print_projected_pose(",") + "," + str(t_time) + "\n"
-            rospy.loginfo("msg_to_controller: " + cont_msg)
+            rospy.logdebug("msg_to_controller: " + cont_msg)
             exec_control_pub(cont_msg)
             publish_path(projection, plan)
 #            rospy.loginfo(str(time.time() - cur_clock))
